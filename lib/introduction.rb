385d4eb0e60ae7d27f128eba6d8d70824bfed302
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-07-31 23:03:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning the program in #{language}."
 end
 
-introduction_with_language(Dan, Ember)
+introduction_with_language()
 
 
 
